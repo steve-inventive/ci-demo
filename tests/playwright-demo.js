da3d4a08f6aa0756e84dbc7d986c0ce34ebd673d
steve-inventive <104204864+steve-inventive@users.noreply.github.com>
2024-09-07 13:27:23 -0400

    Update playwright-demo.js

--- tests/playwright-demo.js
+++ tests/playwright-demo.js
@@ -1,7 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check button exists on CI Demo', async ({ page }) => {
-  await page.goto('https://<username>.github.io/ci-demo');
+test('Check button exists on CI Demo', async ({ browser }) => {
+  const context = await browser.newContext({ headless: true });
+  const page = await context.newPage();
+  await page.goto('https://steve-inventive.github.io/ci-demo');
   const button = await page.$('#demo-button');
   expect(button).not.toBeNull();
 });
