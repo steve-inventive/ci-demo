63ee462678213230a8969fe7d8c84f4e7b2c5020
steve-inventive <104204864+steve-inventive@users.noreply.github.com>
2024-09-09 09:06:20 -0400

    Update testcafe-demo.js
    added extra check into the mix

--- tests/testcafe-demo.js
+++ tests/testcafe-demo.js
@@ -1,9 +1,17 @@
 import { Selector } from 'testcafe';
 
 fixture `CI Demo Website`
-  .page `https://steve-inventive.github.io/ci-demo`;
+  .page `https://steve-inventive.github.io/ci-demo`;  // Replace with the correct URL
 
-test('Check button exists', async t => {
+test('Check button exists and h1 text is correct', async t => {
     const button = Selector('#demo-button');
+    const header = Selector('h1');
+
+    // Check if the <h1> tag contains the correct text
+    await t.expect(header.innerText).eql('Welcome to CI Demo Website');
+    console.log('header is correct');
+
+    // Check if the button exists
     await t.expect(button.exists).ok();
+    console.log('button was found');  
 });
