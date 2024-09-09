4b9f0dc06cb52ab2d43c89e10d56839d53d1a0a1
steve-inventive <104204864+steve-inventive@users.noreply.github.com>
2024-09-09 09:07:18 -0400

    Update playwright-demo.spec.js
    added additional test step

--- tests/playwright-demo.spec.js
+++ tests/playwright-demo.spec.js
@@ -1,9 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check button exists on CI Demo', async ({ browser }) => {
-  const context = await browser.newContext({ headless: true });
-  const page = await context.newPage();
-  await page.goto('https://steve-inventive.github.io/ci-demo');
-  const button = await page.$('#demo-button');
-  expect(button).not.toBeNull();
+test('Check button and h1 text on CI Demo', async ({ page }) => {
+  await page.goto('https://steve-inventive.github.io/ci-demo');  
+
+  // Check if the <h1> tag contains the correct text
+  const headerText = await page.textContent('h1');
+  expect.soft(headerText).toBe('Welcome to Demo Website');
+  console.log('made it past the H1 text check');
+
+  // Check if the button exists
+  const button = await page.$('#demo-button-fail');
+  expect.soft(button).not.toBeNull();
+  console.log('passing button check');
+
 });
